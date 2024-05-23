e7cfb9f3857d54d30e11f3ba0a2cf02ec8213140
今村エリカ <k023g1045@g.neec.ac.jp>
2024-05-23 11:51:49 +0900

    01_03_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,19 +1,90 @@
 #include "GameScene.h"
+#include "ImGuiManager.h"
 #include "TextureManager.h"
 #include <cassert>
 
+#include "AxisIndicator.h"
+#include "PrimitiveDrawer.h"
+
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete sprite_;
+	delete model_;
+	delete debugCamera_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+	// スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, {100, 100});
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	// サウンドデータの読み込み
+	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
+	// 音声再生
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
+	// ライン描画が参照するビュープロジェクションを指定する（アドレス渡し）
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+
+	// デバッグカメラの生成
+	debugCamera_ = new DebugCamera(WinApp::kWindowWidth, WinApp::kWindowHeight);
+
+	// 軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	// 軸方向表示が参照するビュープロジェクションを指定する（アドレス渡し）
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	Vector2 position = sprite_->GetPosition();
+	// 座標移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+
+	// スペースキーを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		// 再生されていたら
+		if (audio_->IsPlaying(voiceHandle_)) {
+			// 音声停止
+			audio_->StopWave(voiceHandle_);
+		} else { // そうでなかったら
+			// 再生(voiceHandle_もう一度読み込み)
+			voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+		}
+	}
+
+// デバッグテキストの表示
+// ImGuiはDebugモード専用(Releaseだとエラー)
+#ifdef _DEBUG // Debugモードの時のみ有効にする
+	ImGui::Text("Kamata Tarou %d.%d.%d.", 2050, 12, 31);
+	ImGui::Begin("Debug1");
+	ImGui::Text("Kamata Tarou %d.%d.%d.", 2050, 12, 31);
+	ImGui::InputFloat3("InputFloat3", inputFloat3);               // float3入力ボックス
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f); // float3スライダー
+	ImGui::End();
+
+	ImGui::ShowDemoWindow();
+#endif
+
+	// デバッグカメラの更新
+	debugCamera_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -42,6 +113,24 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
+	// モデルを連動
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+
+	// ライン描画
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
+
+	// for文2つ使って格子状描画可能
+	/* float lineMeshWidth = 50;
+	float lineMeshHeight = 50;
+	float lineMeshSpanH = 5;
+	float lineMeshSpanV = 5;
+	for (float h = lineMeshWidth / 2; h <= lineMeshWidth / 2; h += lineMeshSpanH) {
+	    PrimitiveDrawer::GetInstance()->DrawLine3d(
+	        {h,-lineMeshHeight/2,0},
+	        {h,lineMeshHeight},
+	    )
+	}*/
+
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -53,6 +142,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+
+	sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,8 @@
 #include "ViewProjection.h"
 #include "WorldTransform.h"
 
+#include "DebugCamera.h"
+
 /// <summary>
 /// ゲームシーン
 /// </summary>
@@ -43,6 +45,20 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+
+	uint32_t textureHandle_ = 0; // テクスチャハンドル
+	Sprite* sprite_ = nullptr;   // スプライト
+
+	Model* model_ = nullptr;        // 3Dモデル
+	WorldTransform worldTransform_; // ワールドトランスフォーム
+	ViewProjection viewProjection_; // ビュープロジェクション
+
+	uint32_t soundDataHandle_ = 0; // サウンドデータハンドル
+	uint32_t voiceHandle_ = 0;     // 音声再生ハンドル
+
+	float inputFloat3[3] = {0, 0, 0}; // ImGuiで値を入力する変数
+
+	DebugCamera* debugCamera_ = nullptr; // デバッグカメラ
 
 	/// <summary>
 	/// ゲームシーン用
